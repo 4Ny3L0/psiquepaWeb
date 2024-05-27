604002cbf4260ea76a8ed1660fd0a3fa2ff63625
Angel Gutierrez <angel50710@hotmail.com>
2024-05-26 22:01:20 -0500

    Agregando validaciones al mobile

--- js/register.js
+++ js/register.js
@@ -1,26 +1,17 @@
 (() => {
   let inputsForm = {
-    first_name: [{ error: false, message: "" }],
-    last_name: [{ error: false, message: "" }],
-    mobile: [{ error: false, message: "" }],
-    email: [{ error: false, message: "" }],
-    document_id: [{ error: false, message: "" }],
-    username: [{ error: false, message: "" }],
-    password: [{ error: false, message: "" }],
-    password_c: [{ error: false, message: "" }],
+    first_name: [{ content: '', message: "Por favor escribe tu nombre" , max_length:20}],
+    last_name: [{ content:'', message: "Por favor escribe tu apellido" , max_length:20}],
+    mobile: [{ content:'', message: "Por favor escribe tu celular" , max_length:8}],
+    email: [{ content:'', message: "Por favor escribe tu correo" , max_length:30}],
+    document_id: [{ content:'', message: "Por favor escribe tu cedula" , max_length:15}],
+    username: [{ content:'', message: "Por favor escribe tu usuario" , max_length:10}],
+    password: [{ content:'', message: "Por favor escribe tu contraseña", max_length:16 }],
+    password_c: [{ content:'', message: "Por favor confirma tu contraseña" , max_length:16}],
   };
+
   document.addEventListener("DOMContentLoaded", () => {
     let registerFormMain = document.querySelector("#register-form");
-    registerFormMain.addEventListener("input", (e) => {
-      for (const inputId of Object.keys(inputsForm)) {
-        let inputElement = document.querySelector(`#${inputId}`);
-        inputsValidations(inputElement, inputId);
-      }
-    });
-    registerFormMain.addEventListener("focusout", (e) => {
-      console.log(e.target.tagName);
-      console.log(inputsForm);
-    });
     registerFormMain.addEventListener("submit", (e) => {
       let loginBtn = document.querySelector("#login-btn");
       e.preventDefault();
@@ -28,39 +19,99 @@
       if (submitBtnIsActive.includes("active")) {
         validateForm();
       }
-      //
     });
+
+    registerFormMain.addEventListener("input",(e)=>{
+      inputsForm[e.target.id][0]['content'] = e.target.value;
+      if(e.target.id == 'mobile'){
+        mobile_custom_input(e);
+      }
+    })
   });
 
-  const validateForm = () => {
-    let registerForm = document.querySelector("#register-form");
-    let registerFormElements = Array.from(registerForm.children);
-    console.log(registerFormElements.length);
-    registerFormElements.forEach((element, index) => {
-      let elementId = element.id;
-      if (elementId != "login-btn") {
-        inputsValidations(element, elementId);
-      }
+
+  const validateForm = ()=>{
+    const is_length_error = length_validations();
+    const is_email_error = email_validations(inputsForm.email);
+    const is_document_id_error = document_id_validation(inputsForm.document_id)
+    if(is_length_error || is_email_error || is_document_id_error){
       return;
-    });
+    }
+    send_data();
   };
 
-  const inputsValidations = (element, elementId) => {
-    let error = false;
-    let messages = "";
-    if (element.value.length <= 0) {
-      error = true;
-      messages = `El campo "${element.placeholder} es requerido"`;
-      inputsForm[elementId][0]["error"] = error;
-      inputsForm[elementId][0]["message"] = messages;
+  const dismissMessage = ()=>{
+    let errorElement = document.querySelector('#error');
+    setTimeout(()=>{
+      errorElement.textContent ='';
+    },2500)
+  }
+
+const length_validations = ()=>{
+  let errorElement = document.querySelector('#error');
+  for(let input of Object.keys(inputsForm)){
+    let actualInput = document.querySelector(`#${input}`)
+    if(inputsForm[input][0].content.length <= 0 ){
+      errorElement.textContent = `${inputsForm[input][0]['message']}`
+      dismissMessage();
       return true;
-    }
-    if (element.value.log > 0) {
-      if (elementId === "username") {
-        usernameValidations(element.value);
-      }
-    }
-  };
-  const usernameValidations = (username) => {};
-  const passwordValidations = () => {};
+  }if(inputsForm[input][0].content.length > inputsForm[input][0].max_length){
+    errorElement.textContent = `Revisa que "${actualInput.placeholder}" sea menor a ${inputsForm[input][0]['max_length']}`;
+    dismissMessage();
+    return true;
+  }
+}
+return false;
+}
+const email_validations = (input)=>{
+  let errorElement = document.querySelector('#error');
+  const regexEmail = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  let email = input[0].content
+  if(email.match(regexEmail) == null){
+    errorElement.textContent = `Revisa que el formato del correo sea valido`;
+    dismissMessage();
+    return true;
+  }
+  return false;
+}
+
+const document_id_validation = (input)=>{
+  let errorElement = document.querySelector('#error');
+  const document_id_regex = /^(?:[1-9]|1[0-3])-(?:[1-9]\d{0,3}|[1-9])-(?:[1-9]\d{0,3}|[1-9])$/;
+  let document_id = input[0].content;
+  if(document_id.match(document_id_regex)==null || document_id.match(document_id_regex)== undefined){
+    errorElement.textContent = `Revisa que el formato de tu cedula sea valido`;
+    dismissMessage();
+    return true;
+  }
+  return false;
+}
+
+const mobile_validations = (e)=>{
+
+}
+
+const mobile_custom_input = (e)=>{
+  e.target.maxLength = 9;
+  let errorElement = document.querySelector('#error');
+  if (e.target.value.length == 4) {
+    e.target.value += '-'
+  }
+  if(e.target.value.length == 5 && e.inputType == 'deleteContentBackward'){
+    let new_value = inputsForm.mobile[0].content.slice('-')
+    e.target.value = new_value
+  }
+  if(e.inputType == 'insertText' && e.target.value.length > 9){
+    // e.preventDefault();
+    // errorElement.textContent = 'Este campo solo permite 8 digitos';
+    // dismissMessage();
+  }
+}
+
+const send_data = ()=>{
+  let errorElement = document.querySelector('#error');
+  errorElement.textContent = 'Enviando datos'
+}
+
+  
 })();
